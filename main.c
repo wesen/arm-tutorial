63ba539d82bc1a590005776445223b8f5b45b1e1
Manuel Odendahl <wesen@ruinwesen.com>
2011-10-05 22:01:11 +0200

    Add main loop (empty for now)

---
+++ main.c
@@ -0,0 +1,20 @@
+/**
+ * Demonstration program for Atmel AT91SAM7S256 processor
+ *
+ * Runs on an olimex SAM7-HXXX board
+ *
+ * Adapter from James P Lynch ARM tutorial
+ **/
+
+#include "board.h"
+
+/**
+ * Main entry point
+ **/
+int main(void) {
+
+  for (;;) {
+  }
+
+  return 0;
+}
